63b6b96e1eba959acc5391587c5b07f7a67832a1
anushakommula645 <anushakommula645@gmail.com>
2025-07-06 23:46:19 +0530

    port number log resolved

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../portfolio/dist/index.html'));
 });
 
-app.listen(5000, () => {
+// Start server (IMPORTANT for Render)
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
